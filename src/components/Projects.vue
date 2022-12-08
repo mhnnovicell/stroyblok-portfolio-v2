7d66f0c9ea06a88f0e82cdab00b383eec8c08e91
AzureAD\MikkelHornbechNielse <mhn@rosidigital.com>
2022-12-08 13:41:19 +0100

    Minor changes to projects slider in the bottom of the page

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -24,78 +24,29 @@
         data-aos="fade-left"
       >
         <Vue3Marquee :pauseOnHover="true">
-          <div
-            class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 p-4 m-8"
-          >
-            <!-- <img
-              :alt="blok?.veganSearchImage.alt"
-              :height="blok?.veganSearchImage.height"
-              :width="blok?.veganSearchImage.width"
-              class="w-32 h-32"
-              v-lazy="{
-                src: require(blok?.veganSearchImage.filename),
-                loading: 'https://via.placeholder.com/150',
-              }"
-            /> -->
-
-            <div class="p-5">
-              <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-              >
-                {{ blok?.veganSearchHeadline }}
-              </h5>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {{ blok?.veganSearchDescription }}
-              </p>
-              <a
-                :href="blok?.veganSearchLink.url"
-                target="_blank"
-                class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Se siden
-                <svg
-                  aria-hidden="true"
-                  class="ml-2 -mr-1 w-4 h-4"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div
-            class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 p-4 m-8"
-          >
-            <!-- <img
+          <div class="max-w-sm bg-indigo-900 rounded-lg shadow p-4 m-8">
+            <img
               :alt="blok?.kurtNImage.alt"
               :height="blok?.kurtNImage.height"
               :width="blok?.kurtNImage.width"
-              class="w-32 h-32"
+              class="h-auto w-1/2 rounded-t-lg object-contain mix-blend-multiply m-auto"
               v-lazy="{
-                src: require(blok?.kurtNImage.filename),
+                src: blok?.kurtNImage.filename,
                 loading: 'https://via.placeholder.com/150',
               }"
-            /> -->
+            />
 
             <div class="p-5">
-              <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-              >
+              <h5 class="text-white mb-2 text-lg font-bold">
                 {{ blok?.kurtNHeadline }}
               </h5>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              <p class="mb-3 font-light text-gray-400">
                 {{ blok?.kurtNDescription }}
               </p>
               <a
                 :href="blok?.kurtNLink.url"
                 target="_blank"
-                class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-green-100 rounded-lg hover:bg-green-100 focus:ring-4 focus:outline-none focus:ring-green-100"
               >
                 Se siden
                 <svg
@@ -114,35 +65,29 @@
               </a>
             </div>
           </div>
-          <div
-            class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-8"
-          >
-            <!-- <img
+          <div class="max-w-sm bg-indigo-900 rounded-lg shadow p-4 m-8">
+            <img
               :alt="blok?.veganSearchImage.alt"
               :height="blok?.veganSearchImage.height"
               :width="blok?.veganSearchImage.width"
-              class="h-auto w-full"
+              class="h-auto w-1/2 rounded-t-lg object-contain mix-blend-multiply m-auto"
               v-lazy="{
                 src: blok?.veganSearchImage.filename,
                 loading: 'https://via.placeholder.com/150',
               }"
-            /> -->
-
-            {{ blok?.veganSearchImage.filename }}
+            />
 
             <div class="p-5">
-              <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-              >
+              <h5 class="text-white mb-2 text-lg font-bold">
                 {{ blok?.veganSearchHeadline }}
               </h5>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              <p class="mb-3 font-light text-gray-400">
                 {{ blok?.veganSearchDescription }}
               </p>
               <a
                 :href="blok?.veganSearchLink.url"
                 target="_blank"
-                class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-green-100 rounded-lg hover:bg-green-100 focus:ring-4 focus:outline-none focus:ring-green-100"
               >
                 Se siden
                 <svg
@@ -164,13 +109,6 @@
         </Vue3Marquee>
       </div>
     </div>
-    <img
-      :src="blok?.kurtNImage.filename"
-      class="w-full h-full"
-      :alt="blok?.kurtNImage.alt"
-      :height="blok?.kurtNImage.height"
-      :width="blok?.kurtNImage.width"
-    />
   </section>
   <!-- Tjek Storyblok documentation mht foreach og lignende ting man tilføje for at gøre det nemmere at tilføje content osv -->
   <!-- Alternativt se den her video: https://www.youtube.com/watch?v=viV0ROCVEO8 https://www.youtube.com/watch?v=zVISvGMz6xc&t=189s -->
